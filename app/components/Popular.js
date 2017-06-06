91d8940f4bf0f44c9db7e89cfedaa55ac8d13029
Dairon Rodriguez <drodriguez@fvi.edu>
2017-06-06 19:02:18 -0400

    github battle done

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -12,7 +12,7 @@ function SelectLanguage(props) {
       {languages.map((lang) => {
         return (
           <li
-            style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+            style={lang === props.selectedLanguage ? { color: #d0021b } : null}
             onClick={props.onSelect.bind(null, lang)}
             key={lang}
           >
